b0cd415d3ee90f8f254dc7a0eec4d3322d17ac57
Korhan Aladağ <50011233+KorhanAladag@users.noreply.github.com>
2023-10-09 15:00:24 +0300

    Add files via upload

---
+++ components/Header.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Header(){
+    return (
+    <div className="header--container">
+        <img src="./images/journal-logo.png" className="header--logo" alt="travel-logo"/>
+        <h2 className="header--title">my travel journal</h2>
+    </div>
+    )
+}
